ae86b568b0723f8f4a14a1d2fbfeba2ce100ccc7
Jacek Kawa <jacek.kawa@polsl.pl>
2021-04-13 13:04:16 +0200

    poprawki stylu

--- TOWAM2021/Program.cs
+++ TOWAM2021/Program.cs
@@ -7,8 +7,7 @@ namespace TOWAM2021
         static void Main(string[] args)
         {
             double a, b, c;
-            double x1, x2;
-
+            
             Console.WriteLine("ax^2+bx+c=0");
             Console.WriteLine("wprowadź a");
             a = Double.Parse(Console.ReadLine());
@@ -21,13 +20,14 @@ namespace TOWAM2021
 
             if (delta > 0)
             {
-
+                double x1, x2;
                 x1 = (-b - Math.Sqrt(delta)) / (2 * a);
                 x2 = (-b + Math.Sqrt(delta)) / 2 / a;
 
                 Console.WriteLine("Pierwiastki to: {0} i {1}", x1, x2);
             } else if (delta == 0)
             {
+                double x1;
                 x1 = (-b) / 2 * a;
                 Console.WriteLine("Jeden podwójny pierwiastek to: {0}", x1);
             }
