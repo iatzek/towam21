f551b8e6809feaa7bc127c240247b4ffd42c2e66
Jacek Kawa <jacek.kawa@polsl.pl>
2021-04-13 12:36:45 +0200

    Początek pracy

---
+++ TOWAM2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TOWAM2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
