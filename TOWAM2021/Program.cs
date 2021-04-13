f6a1ff71db3687649f50237803c6aade41cb485f
Jacek Kawa <jacek.kawa@polsl.pl>
2021-04-13 12:43:22 +0200

    Pierwsza działająca wersja

--- TOWAM2021/Program.cs
+++ TOWAM2021/Program.cs
@@ -6,7 +6,16 @@ namespace TOWAM2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 1, b = -5, c = -10;
+            double x1, x2;
+
+            double delta = b * b - 4 * a * c;
+
+            x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+            Console.WriteLine("Pierwiastki to: {0} i {1}", x1, x2);
+
         }
     }
 }
